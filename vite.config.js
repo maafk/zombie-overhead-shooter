14dc8c25bc2c414392bdc94a637ad1bfc0d2ae5a
Taylor Ondrey <taylor@taylorondrey.com>
2025-07-12 07:02:49 -0400

    fix base after repo rename

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/muh-game/',
+  base: '/zombie-overhead-shooter/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
